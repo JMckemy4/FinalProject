cc88436f1d34b314d6e0a9ba759c9c13038698a3
alimarcanotovar <aliymarcanotovar91@gmail.com>
2022-09-27 14:52:38 -0500

    bored api

---
+++ AspBoredApi/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using AspBoredApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspBoredApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+       
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var api = new ApiCall();
+            var activity = api.GetActivity();
+            return View(activity);
+        }
+
+        public IActionResult ViewActivity()
+        {
+            var api = new ApiCall();
+            var act = api.GetActivity();
+            return View(act);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspBoredApi/Models/ApiCall.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+
+namespace AspBoredApi.Models
+{
+    public class ApiCall
+    {
+        public ActivityProperties GetActivity()
+        {
+
+
+            var client = new HttpClient();
+            var BoredApiUrl = "http://www.boredapi.com/api/activity/";
+            var BoredApiResponse = client.GetStringAsync(BoredApiUrl).Result;
+            var boredObject = JObject.Parse(BoredApiResponse);
+
+            return new ActivityProperties()
+            {
+                Activity = boredObject["activity"].ToString(),
+                Type = boredObject["type"].ToString(),
+
+            };
+            //var BoredApiResult = JObject.Parse(BoredApiResponse).GetValue("activity").ToString();
+            //return (BoredApiResult);
+        }
+    }
+}
+
+
+
+
+
+
+
